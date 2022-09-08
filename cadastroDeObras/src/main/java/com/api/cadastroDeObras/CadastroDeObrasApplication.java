cb4ef311d0ffa1f018f1898749dcbb5ebd2095aa
Leonardo Francis <leonardoprofissionaljob@gmail.com>
2022-09-08 03:25:09 -0300

    Base da aplicação e docker-compose

---
+++ cadastroDeObras/src/main/java/com/api/cadastroDeObras/CadastroDeObrasApplication.java
@@ -0,0 +1,13 @@
+package com.api.cadastroDeObras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeObrasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeObrasApplication.class, args);
+	}
+
+}
---
+++ cadastroDeObras/src/test/java/com/api/cadastroDeObras/CadastroDeObrasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.cadastroDeObras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeObrasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
