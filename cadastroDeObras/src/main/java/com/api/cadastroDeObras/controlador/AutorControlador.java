2e3cf30ef83493edc007c504b683de8430384aab
Leonardo Francis <leonardoprofissionaljob@gmail.com>
2022-09-12 13:02:03 -0300

    Autor: deletar, cadastrar e atualizar
    - listarAutorPorCodigo
    - deletarAutor;
    - cadastrarAutor;
    - atualizarAutor;
    - validarExistenciaAutor.

--- cadastroDeObras/src/main/java/com/api/cadastroDeObras/controlador/AutorControlador.java
+++ cadastroDeObras/src/main/java/com/api/cadastroDeObras/controlador/AutorControlador.java
@@ -3,9 +3,18 @@ package com.api.cadastroDeObras.controlador;
 import com.api.cadastroDeObras.entidades.Autor;
 import com.api.cadastroDeObras.servico.AutorServico;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,5 +28,25 @@ public class AutorControlador {
     public List<Autor> listarAutor(){
         return autorServico.listarAutor();
     }
-            
+      
+    @GetMapping("/{codigo}")
+    public Optional<Autor> listarAutorPorCodigo(@PathVariable Long codigo) {
+        return autorServico.listarAutorPorCodigo(codigo);
+    }
+    
+    @PostMapping
+    public ResponseEntity<Autor> cadastrarAutor(@RequestBody Autor autor) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(autorServico.cadastrarAutor(autor));
+    }
+    
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{codigo}")
+    public void deletarAutor(@PathVariable Long codigo) {
+        autorServico.deletarAutor(codigo);
+    }
+    
+    @PutMapping("/{codigo}")
+    public ResponseEntity<Autor> atualizarAutor(@PathVariable Long codigo, @RequestBody Autor autor) {
+        return ResponseEntity.status(HttpStatus.OK).body(autorServico.atualizarAutor(codigo, autor));
+    }
 }
--- cadastroDeObras/src/main/java/com/api/cadastroDeObras/servico/AutorServico.java
+++ cadastroDeObras/src/main/java/com/api/cadastroDeObras/servico/AutorServico.java
@@ -3,7 +3,10 @@ package com.api.cadastroDeObras.servico;
 import com.api.cadastroDeObras.entidades.Autor;
 import com.api.cadastroDeObras.repositorio.AutorRepositorio;
 import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,7 +15,33 @@ public class AutorServico {
     @Autowired
     private AutorRepositorio autorRepositorio;
     
-    public List<Autor> listarAutor(){
+    public List<Autor> listarAutor() {
         return autorRepositorio.findAll();
+    }
+    
+    public Optional<Autor> listarAutorPorCodigo(Long codigo) {
+        return autorRepositorio.findById(codigo);
+    }
+    
+    public Autor cadastrarAutor (Autor autor) {
+        return autorRepositorio.save(autor);
+    }
+    
+    public void deletarAutor (Long codigo) {
+        autorRepositorio.deleteById(codigo);
+    }
+    
+    public Autor atualizarAutor (Long codigo, Autor autor) {
+        Autor autorAtualizado = validarExistenciaAutor(codigo);
+        BeanUtils.copyProperties(autor, autorAtualizado, "codigo");
+        return autorRepositorio.save(autorAtualizado);
+    }
+    
+    public Autor validarExistenciaAutor (Long codigo) {
+        Optional<Autor> autor = listarAutorPorCodigo(codigo);
+        if (autor.isEmpty()) {
+            throw new EmptyResultDataAccessException(1);
+        }
+        return autor.get();
     }
 }
